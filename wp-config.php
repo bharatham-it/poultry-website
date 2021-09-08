ea4eb5e413bbc4436f63f16cafad221472e155bc
bharatham-it <bharau7m@md-3.webhostbox.net>
2021-09-08 05:11:31 +0000

    contact form 7 has added

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'poultry_web' );
+define( 'DB_NAME', 'bharau7m_joys_website' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'bharau7m_joys_website' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'jvWLtrt}J$DW' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
